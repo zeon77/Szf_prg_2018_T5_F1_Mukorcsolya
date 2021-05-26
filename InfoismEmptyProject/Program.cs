e37567d812828e3f5dc5975013f373d3ae79d3e8
zeon77 <zeon77@gmail.com>
2021-05-26 20:35:18 +0200

    3. feladat

--- InfoismEmptyProject/Program.cs
+++ InfoismEmptyProject/Program.cs
@@ -10,20 +10,24 @@ namespace Helsinki2017
     {
         static void Main(string[] args)
         {
-            List<Eredmény> erdemények = new List<Eredmény>();
+            List<Eredmény> eredmények = new List<Eredmény>();
 
             foreach (var line in File.ReadAllLines("rovidprogram.csv").Skip(1))
             {
-                erdemények.Add(new Eredmény(line, "rovid"));
+                eredmények.Add(new Eredmény(line, "rovid"));
             }
 
             foreach (var line in File.ReadAllLines("donto.csv").Skip(1))
             {
-                erdemények.Add(new Eredmény(line, "donto"));
+                eredmények.Add(new Eredmény(line, "donto"));
             }
 
             //2. feladat
-            Console.WriteLine($"2. feladat\n\tA rövidprogramban {erdemények.Where(x => x.Szakasz == "rovid").Count()} induló volt.");
+            Console.WriteLine($"2. feladat\n\tA rövidprogramban {eredmények.Where(x => x.Szakasz == "rovid").Count()} induló volt.");
+
+            //3.
+            string s = eredmények.Where(x => x.Szakasz == "donto").Any(x => x.Ország == "HUN") ? "bejutott" : "nem jutott be";
+            Console.WriteLine($"3. feladat\n\t A magyar versenyző {s} a kűrbe.");
 
             Console.ReadKey();
         }
